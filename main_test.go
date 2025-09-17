5fc0e42a8dcc4ec27446dbea4a073c83a0f3504c
Simon Tucker <brotherlogic@gmail.com>
2025-09-16 17:30:18 -0700

    Added basic pg test

--- main_test.go
+++ main_test.go
@@ -1,20 +1,46 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"testing"
 
-	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stapelberg/postgrestest"
 )
 
-func createTestServer() *server {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-	}
-	defer db.Close()
+var pgt *postgrestest.Server
 
+func TestMain(m *testing.M) {
+	var err error
+	pgt, err = postgrestest.Start(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	defer pgt.Cleanup()
+
+	m.Run()
 }
 
 func TestInitDB(t *testing.T) {
+	pgurl, err := pgt.CreateDatabase(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+	db, err := sql.Open("postgres", pgurl)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
 
+	s := &server{db: db}
+	s.initDB()
+
+	// Check that we've built to version2
+	version, err := s.checkDBVersion()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if version != "2" {
+		t.Errorf("Bad db version: %v", version)
+	}
 }
